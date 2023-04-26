8c03c76ca915657ac9d956b719187d81c386e319
Sebastián Félix Gorga <sebagorga@gmail.com>
2023-04-26 12:54:26 +0200

    Added the UML file and JavaDoc

---
+++ project_JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"business"},{"l":"business.entities"},{"l":"persistence"},{"l":"persistence.exceptions"},{"l":"persistence.json.adventure"},{"l":"persistence.json.character"},{"l":"persistence.json.monster"},{"l":"presentation"},{"l":"presentation.views"},{"l":"presentation.views.console"}];updateSearchResults();
